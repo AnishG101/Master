bfb639473bcb7f91f6bd8b2a8e01c8ae8e1b0e8f
Anish Ghai <anish11ghai@gmail.com>
2017-05-15 23:39:05 +1000

    Add files via upload

---
+++ FPS/Scripts/EnemyHealth.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+	public int startingHealth = 100;
+	public int currentHealth;
+	bool isDead = false;
+	public GameObject gameObject;
+	ParticleSystem ps;
+	// Use this for initialization
+	void Start () {
+		currentHealth = startingHealth;
+		ps = GetComponentInChildren<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void TakeDamage(int amount, Vector3 hitPoint) {
+		if (isDead) {
+			return;
+		}
+			
+		currentHealth -= amount;
+
+		if (currentHealth <= 0) { 
+			Death ();
+		}
+
+	}
+
+	void Death() {
+		isDead = true;
+		Destroy (gameObject);
+		ps.Play ();
+	}
+}
